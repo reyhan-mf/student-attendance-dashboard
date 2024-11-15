e57a6160654ad8f79104439685202bb122b6b7a5
Reyhan M Fabian <rey.softeng@gmail.com>
2024-11-15 20:41:23 +0700

    added daftar absen, daftar siswa, fixed js sccroll to the top

--- assets/js/reyhan.js
+++ assets/js/reyhan.js
@@ -1,4 +1,6 @@
-function updateTitle(element) {
-    const button = element.closest('.btn-group').querySelector('.dropdown-toggle');
-    button.textContent = element.textContent;
-  }
+function updateTitle(event) {
+    event.preventDefault(); // Mencegah tindakan default
+    const element = event.target; // Elemen yang diklik
+    const button = element.closest('.btn-group').querySelector('.dropdown-toggle'); // Tombol dropdown terdekat
+    button.textContent = element.textContent; // Perbarui teks tombol
+}
